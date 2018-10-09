34349e3017cf0f6e32de14065d456edd084cbb09
Renaldo Baur Filho <renaldobf@gmail.com>
2018-10-09 00:57:34 -0300

    Submissão inicial

---
+++ README.md
@@ -0,0 +1,76 @@
+Aracnator
+=========
+
+   Aracnator é um sistema especialista que reconhece a espécie de
+uma aranha a partir de perguntas respondidas pelo usuário.
+
+   O sistema monta uma árvore de decisão a partir de uma base
+de conhecimento com informações sobre diferentes espécies de aranhas.
+
+Introdução
+----------
+
+   De acordo com o Ministério da Saúde, entre 2010 e 2014 foram
+notificados 691.307 acidentes por animais peçonhentos, dos quais
+1.282 evoluíram para óbito. Aranhas foram responsáveis por 27.119
+dessas notificações, sendo 13 óbitos.
+Fonte: (Acidentes por Animais Peçonhentos)[http://portalarquivos.saude.gov.br/images/pdf/2016/maio/20/Informe-Epidemiol--gico-animais-pe--onhentos---.pdf]
+
+   Foram selecionadas espécies de aranhas e pesquisados dados
+morfológicos, de hábitos e distribuição geográfica. O arquivo
+`base.json` com a compilação dessas informações deve estar junto
+deste script para o funcionamento correto do sistema.
+
+   A ideia original foi montada usando o Expert SINTA. O arquivo
+`aranhas.bcm` ainda pode ser visualizado na pasta. 
+
+   O SINTA ajudou a formular a ideia original, mas foi abandonado
+porque, apesar de sua interface fácil, o sistema atingiu 15 regras
+após a inserção de informações sobre 3 espécies, que o tornaria
+difícil de gerenciar e corrigir para um número maior de espécies.
+
+
+Funcionamento
+-------------
+
+   Para construção da árvore de decisões é utilizado o algoritmo C4.5,
+desenvolvido por [Ross Quinlan](http://www.rulequest.com/Personal/).
+A implementação mostrada aqui foi adaptada a partir do código de
+[Timofey Trukhanov](https://github.com/geerk/C45algorithm) em Python.
+
+   Após a construção de um nível da árvore, a pergunta com maior
+ganho de informação é exibida ao usuário. Ao responder, as classes
+pertencentes o outro ramo são descartadas e o algoritmo procede
+para o próximo nó.
+
+   Caso o usuário não tenha a resposta para uma pergunta, em vez de
+assumir um valor padrão, é gerada uma nova árvore de decisões ignorando
+a variável desconhecida.
+
+   Ao chegar ao nó folha, são exibidas as possíveis respostas. Se duas
+ou mais resposta contradizerem os dados da base de conhecimento, ocorre
+um impasse e é dado um aviso ao usuário.
+
+Execução
+--------
+
+   O arquivo está formatado para ser executado diretamente, usando
+o Node.js.
+
+	node aracnator.js
+
+Possíveis melhorias
+-------------------
+
+   Os dados foram coletados de forma amadora, apenas para provar o
+conceito, sendo necessária uma validação por um biólogo antes que o
+sistema possa ser empregado em um cenário real.
+
+   Com ajuda de um especialista, poderia ser possível distinguir 
+diferentes espécies dentro do mesmo gênero e inclusive diferenciar
+entre espécimes machos e fêmeas.
+
+   O sistema também não leva em conta a probabilidade de encontro,
+com a espécie (com base na população estimada, por exemplo), o que
+seria importante se o sistema for classificar um número maior de
+espécies.
---
+++ aracnator.js
@@ -0,0 +1,279 @@
+// Carrega a base de conhecimento
+var base = require('./base.json');
+// Estado de trabalho uma consulta
+var state;
+
+/*
+	Faz uma cópia profunda (clone) de um objeto.
+*/
+function clone(item) {
+	return JSON.parse(JSON.stringify(item));
+}
+
+/*
+	Remove valores duplicados de um array.
+*/
+function deldup(array) {
+	return array.filter((v,i,a)=>a.indexOf(v)===i).sort((a,b)=>a>b);
+}
+
+/*
+	Processa a base de conhecimento, separando as variáveis e os valores.
+*/
+function processar(base) {
+	let variaveis = [];
+	let valores = [];
+	let intervalos = [];
+
+	// Adiciona as variáveis de "perguntas"
+	for (let i=0; i<base.perguntas.length; i++)
+		if (variaveis.indexOf(base.perguntas[i].variavel) < 0)
+			variaveis.push(base.perguntas[i].variavel)
+	
+	// Busca variáveis não definidas em "perguntas"
+	for (let i=0; i<base.saidas.length; i++) {
+		let c = base.saidas[i].entradas;
+		for (let j=0; j<c.length; j++) {
+			// Adicina nova variável
+			if (variaveis.indexOf(c[j].variavel) < 0)
+				variaveis.push(c[j].variavel);
+			// Verifica se já existem valores
+			if (!valores[c[j].variavel])
+				valores[c[j].variavel] = [];
+			// Adiciona valores de Arrays
+			if (c[j].valor instanceof Array)
+				valores[c[j].variavel] = valores[c[j].variavel].concat(c[j].valor);
+			// Adiciona valores soltos
+			else
+				valores[c[j].variavel].push(c[j].valor);
+			// Verifica se a variável é pode assumir valores em um intervalor
+			if (c[j].intervalo)
+				intervalos.push(c[j].variavel);
+		}
+	}
+	// Remove valores duplicados
+	for (let i=0; i<variaveis.length; i++)
+		valores[variaveis[i]] = deldup(valores[variaveis[i]]);
+
+	base.variaveis = variaveis;
+	base.valores = valores;
+	base.intervalos = deldup(intervalos);
+}
+
+/*
+	Função que verifica se o valor da resposta corresponde à saída
+*/
+function verifica(saida, variavel, valor) {
+	let i;
+	for (i=0; i<saida.entradas.length; i++)
+		if (saida.entradas[i].variavel == variavel) break;
+	if (i >= saida.entradas.length)
+		return false;
+	if (saida.entradas[i].valor instanceof Array) {
+		if (saida.entradas[i].intervalo)
+			return valor >= saida.entradas[i].valor[0] && valor <= saida.entradas[i].valor[1];
+		else
+			return saida.entradas[i].valor.indexOf(valor) >= 0;
+	}
+	return saida.entradas[i].valor == valor;
+}
+
+/*
+	Cria novas saídas sem uma variável
+*/
+function del_values(saidas, variavel, valor) {
+	let novas_saidas = clone(saidas); //.slice(0);
+	for (let i=0; i<novas_saidas.length; i++) {
+		if (valor != null && !verifica(novas_saidas[i], variavel, valor)) {
+			novas_saidas.splice(i,1);
+			i--;
+			continue;
+		}
+		for (let j=0; j<novas_saidas[i].entradas.length; j++) {
+			if (novas_saidas[i].entradas[j].variavel == variavel) {
+				novas_saidas[i].entradas.splice(j,1);
+				j--;
+			}
+		}
+	}
+	return novas_saidas;
+}
+
+/*
+	Retorna os possíveis valores de uma variável dadas as
+	possíveis saídas.
+*/
+function get_valores(saidas, variavel) {
+	let valores = [];
+	for (let i=0; i<saidas.length; i++) {
+		let c = saidas[i].entradas;
+		for (let j=0; j<c.length; j++) {
+			if (c[j].variavel != variavel)
+				continue;
+			if (c[j].valor instanceof Array)
+				valores = valores.concat(c[j].valor);
+			else
+				valores.push(c[j].valor);
+		}
+	}
+	valores = deldup(valores);
+	return valores;
+}
+
+/*
+	Retorna as saídas divididas pelos possíveis valores da variável.
+*/
+function get_subtables(saidas, variavel) {
+	let valores = get_valores(saidas, variavel);
+
+	let subtables = [];
+	for (let i=0; i<valores.length; i++) {
+		subtables.push(del_values(saidas, variavel, valores[i]));
+	}
+	return subtables;
+}
+
+/*
+	Calcula a entropia das saídas para uma variável de entrada.
+*/
+function info(saidas) {
+	let soma = 0;
+	for (let i=0; i<saidas.length; i++) {
+		let p = 1 / saidas.length;
+		soma += p * Math.log2(p);
+	}
+	return -soma;
+}
+
+/*
+	Calcula a entropia das saídas após dividir o nó baseado nos
+	possíveis valores de uma variável de entrada.
+*/
+function infox(saidas, variavel) {
+	let soma = 0;
+	let subt = get_subtables(saidas, variavel);
+	for (let i=0; i<subt.length; i++)
+		soma += subt[i].length / saidas.length * info(subt[i]);
+	return soma;
+}
+
+/*
+	Função que calcula o ganho de informação de uma alternativa.
+*/
+function gain(saidas, variavel) {
+	return info(saidas) - infox(saidas, variavel);
+}
+
+/*
+	Ponto de entradas do algoritmo C45.
+*/
+function c45(saidas, variaveis) {
+	let max = 0, max_variable = null;
+	for (let i=0; i < variaveis.length; i++) {
+		let v = variaveis[i];
+		let g = gain(saidas, v);
+		if (g > max) {
+			max = g;
+			max_variable = v;
+		}
+	}
+	return max_variable;
+}
+
+/*
+	Cria um novo estado de trabalho
+*/
+function reset_state() {
+	state = clone(base);
+}
+
+/*
+	Cria um novo nó na árvore.
+*/
+function iterate() {
+	if (state.saidas.length == 0) {
+		console.log('Os dados informados não correspondem a nenhuma saída possível.');
+		process.exit(0);
+		return;
+	}
+	if (state.saidas.length == 1) {
+		console.log('Resposta encontrada:');
+		console.log(state.saidas[0].valor);
+		process.exit(0);
+		return;
+	}
+	let variavel = c45(state.saidas, state.variaveis);
+	if (!variavel) {
+		console.log('Nenhuma resposta encontrada');
+		console.log(state.saidas.length+' possíveis valores');
+		console.log(state.saidas.map((a)=>a.valor).join(', '));
+		process.exit(0);
+		return;
+	}
+	state.valores[variavel] = get_valores(state.saidas, variavel);
+	faz_pergunta(variavel);
+}
+
+/*
+	Exibe uma pergunta ao usuário
+*/
+function faz_pergunta(variavel) {
+	let pergunta = 'Informe o valor para a variável "' + variavel+'": ';
+	for (let i=0; i<base.perguntas.length; i++) {
+		if (base.perguntas[i].variavel == variavel)
+			pergunta = base.perguntas[i].pergunta;
+	}
+	let opcoes;
+	if (base.intervalos.indexOf(variavel) >= 0)
+		opcoes = 'Valores entre ('+state.valores[variavel][0]+')'
+			+' e ('+state.valores[variavel][state.valores[variavel].length-1]+')';
+	else
+		opcoes = state.valores[variavel].join(', ');
+	state.variavel = variavel;
+	state.resposta = null;
+	console.log(pergunta+'\nOpções: '+opcoes+' ou NDA');
+}
+
+/*
+	Processa a entrada do usuário
+*/
+function processa_entrada(str) {
+	if (!state.variavel) return;
+	str = str.toLowerCase();
+	if (parseInt(str) == str)
+		str = parseInt(str);
+	if (str == 'nda') {
+		state.saidas = del_values(state.saidas, state.variavel, null);
+	}
+	else if (!(
+		typeof str === 'number'
+		&&
+		base.intervalos.indexOf(state.variavel) >= 0
+		&&
+		str >= state.valores[state.variavel][0]
+		&&
+		str <= state.valores[state.variavel][state.valores[state.variavel].length-1]
+		||
+		base.valores[state.variavel].indexOf(str) >= 0
+	)) {
+		console.log('Opção inválida!');
+		faz_pergunta(state.variavel);
+		return;
+	}
+	else {
+		state.saidas = del_values(state.saidas, state.variavel, str);
+	}
+	state.variaveis.splice(state.variaveis.indexOf(state.variavel),1);
+	iterate();
+}
+
+/*
+	Função que lê a entrada padrão
+*/
+process.openStdin().addListener("data", function(d) {
+	processa_entrada(d.toString().trim());
+});
+
+processar(base);
+reset_state();
+iterate();
